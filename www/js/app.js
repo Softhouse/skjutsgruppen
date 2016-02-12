929415f1b1e9b9d3bb752db3a67e8daecb72bbaa
unknown <ton@TON.softhouse.se>
2016-02-12 19:54:54 +0100

    Added the html-site for registrating a trip as a driver

--- www/js/app.js
+++ www/js/app.js
@@ -32,6 +32,15 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
+  .state('app.registerTripDriver', {
+    url: '/registerTripDriver',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/registerTripDriver.html'
+      }
+    }
+  })
+
   .state('app.search', {
     url: '/search',
     views: {
