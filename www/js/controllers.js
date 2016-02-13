6fde9ed8733da74099ed146a85b720e739955cbd
Stellan Lindell <stiltet@gmail.com>
2016-02-13 03:09:10 +0100

    Refactor.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,130 +1,121 @@
 /* global google */
 angular.module('skjutsgruppen.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, $state) {
+    .controller('AppCtrl', function ($scope, $ionicModal, $timeout, $state) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
+        // Form data for the login modal
+        $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+        // Triggered in the login modal to close it
+        $scope.closeLogin = function () {
+            $scope.modal.hide();
+        };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+        // Open the login modal
+        $scope.login = function () {
+            $scope.modal.show();
+        };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+        // Perform the login action when the user submits the login form
+        $scope.doLogin = function () {
+            console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+            // Simulate a login delay. Remove this and replace with your login
+            // code if using a login system
+            $timeout(function () {
+                $scope.closeLogin();
+            }, 1000);
+        };
 
-  $scope.goHome = function () {
-      $state.go('app.startpage');
-  };
-})
+        $scope.goHome = function () {
+            $state.go('app.startpage');
+        };
+    })
 
-.controller('registerTripDriverCtrl', function($scope, $state) {
-  $scope.myForm = {};
-  var listOfViaLocations = [];
+    .controller('registerTripDriverCtrl', function ($scope, $state) {
+        $scope.myForm = {};
+        var listOfViaLocations = [];
 
-  $scope.addTripDriver = function(form) {
-      // console.log("listOfViaLocations: " + listOfViaLocations);
-      // console.log("form:");
-      // console.log(form);
-      // console.log("$scope.myForm:");
-      // console.log($scope.myForm);
-      //console.log($state.get());
-      // alert('Add trip to list');
+        $scope.addTripDriver = function (form) {
+            // console.log("listOfViaLocations: " + listOfViaLocations);
+            // console.log("form:");
+            // console.log(form);
+            // console.log("$scope.myForm:");
+            // console.log($scope.myForm);
+            //console.log($state.get());
+            // alert('Add trip to list');
 
-      window.localStorage['result'] = JSON.stringify($scope.myForm);
-      // var result = JSON.parse(window.localStorage['result'] || '{}');
-      // console.log("result: ");
-      // console.log(result);
+            window.localStorage['result'] = JSON.stringify($scope.myForm);
+            // var result = JSON.parse(window.localStorage['result'] || '{}');
+            // console.log("result: ");
+            // console.log(result);
 
 
-      $state.go('app.summary');
-  };
+            $state.go('app.summary');
+        };
 
-  $scope.addLocation = function() {
-      listOfViaLocations.push($scope.myForm.viaLocation);
-  };
-})
+        $scope.addLocation = function () {
+            listOfViaLocations.push($scope.myForm.viaLocation);
+        };
+    })
 
-.controller('StartpageCtrl', function($scope) {
-})
+    .controller('StartpageCtrl', function ($scope) {
+    })
 
-.controller('ResultCtrl', function($scope) {
+    .controller('ResultCtrl', function ($scope) {
 
-})
+    })
 
-.controller('SummaryCtrl', function($scope) {
+    .controller('SummaryCtrl', function ($scope) {
 
-  var result = JSON.parse(window.localStorage['result'] || '{}');
-  // console.log("result: ");
-  // console.log(result);
-  $scope.listHasLocations = false;
-  $scope.item = 
-    {from: result.from, to: result.to, firstTime: result.firstTime, secondTime: result.secondTime, availableSeats: result.availableSeats, viaLocation: result.viaLocation};
-
-    var listOfViaLocations = result.viaLocation;
-    console.log(listOfViaLocations);
-    // console.log("nr: " + result.viaLocation.length);
-
-    if(listOfViaLocations.size > 1){
-        $scope.listHasLocations = true;
-    }
-    else{
+        var result = JSON.parse(window.localStorage['result'] || '{}');
+        // console.log("result: ");
+        // console.log(result);
         $scope.listHasLocations = false;
-    }
-  // for each()
+        $scope.item =
+        { from: result.from, to: result.to, firstTime: result.firstTime, secondTime: result.secondTime, availableSeats: result.availableSeats, viaLocation: result.viaLocation };
 
-    // {from: result.from},
-    // {title: "Info 2"},
-    // {title: "Info 3"},
-    // {title: "Info 4"},
-    // {title: "Info 5"},
+        var listOfViaLocations = result.viaLocation;
+        console.log(listOfViaLocations);
+        // console.log("nr: " + result.viaLocation.length);
+
+        if (listOfViaLocations.size > 1) {
+            $scope.listHasLocations = true;
+        }
+        else {
+            $scope.listHasLocations = false;
+        }
+        // for each()
+
+        // {from: result.from},
+        // {title: "Info 2"},
+        // {title: "Info 3"},
+        // {title: "Info 4"},
+        // {title: "Info 5"},
   
 
-  // $scope.data = {
-  //   showReordering: false
-  // }
-})
+        // $scope.data = {
+        //   showReordering: false
+        // }
+    })
 
-
-
-
-
-
-
-
-
-
-    .controller('AchievementsCtrl', function ($scope, AchievementFactory) {
-        AchievementFactory.getAchievements().all().then(function(data) {
-          $scope.achievements = data;
+    .controller('AchievementsCtrl', function ($scope, AchievementsFactory) {
+        AchievementsFactory.getAchievements().all().then(function (data) {
+            $scope.achievements = data;
         });
     })
 
@@ -200,13 +191,13 @@ angular.module('skjutsgruppen.controllers', [])
         })
     })
 
-    .controller('SettingsCtrl', function ($scope, SettingFactory) {
+    .controller('SettingsCtrl', function ($scope, SettingsFactory) {
 
-        SettingFactory.getSettings().all().then(function(data) {
-          $scope.settings = data;
+        SettingsFactory.getSettings().all().then(function (data) {
+            $scope.settings = data;
         });
 
         $scope.toggleSetting = function (settingId, newValue) {
-            SettingFactory.updateSetting(settingId, newValue);
+            SettingsFactory.updateSetting(settingId, newValue);
         }
     });
